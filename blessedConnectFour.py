77440bf1630f79514c147f036d6ccd6be1da5258
vguo2037 <86543229+vguo2037@users.noreply.github.com>
2021-07-12 11:15:00 +1000

    Update blessedConnectFour.py

--- blessedConnectFour.py
+++ blessedConnectFour.py
@@ -1,18 +1,17 @@
 class ConnectFour:
     import numpy as np
+    import time
     from blessed import Terminal
 
     term = Terminal()
-    style_bkg = term.color_rgb(0, 0, 255)
-    style_line = term.color_rgb(255, 255, 255)
-    style_p1 = term.color_rgb(255, 255, 0)
-    style_p2 = term.color_rgb(255, 0, 0)
+    style_default = term.white_on_blue
+    style_p1 = term.yellow_on_blue
+    style_p2 = term.red_on_blue
 
     min_nRow_nCol = 4
     max_nCol = 26
     COL_SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     TITLE = "Connect Four"
-    TITLE_ERASE = "            "
 
     ################################################################
     with term.cbreak(), term.hidden_cursor():
@@ -20,7 +19,6 @@ class ConnectFour:
         print(term.home + term.white_on_blue + term.clear)
 
         title = term.move_xy(term.width//2-len(TITLE)//2, term.height//2-len(TITLE)//2) + TITLE
-        title_erase = term.move_xy(term.width//2-len(TITLE)//2, term.height//2-len(TITLE)//2) + TITLE_ERASE
         print(title, end="", flush=True)
 
         start_txt = False
@@ -34,10 +32,7 @@ class ConnectFour:
                 print(start_erase, end="", flush=True)
                 start_txt = False
 
-        print(title_erase, end="", flush=True)
-        print(start_erase, end="", flush=True)
-
-    print(term.home + term.white_on_blue + term.clear)
+    print(term.home + term.clear)
 
     ################################################################
     def __init__(self):
@@ -61,9 +56,7 @@ class ConnectFour:
             except ValueError:
                 error = self.term.move_xy(self.term.width//2-18, self.term.height//3) + \
                         "Please use the format HEIGHT x WIDTH."
-                error_erase = self.term.move_xy(self.term.width//2-18, self.term.height//3) + \
-                        "                                     "
-                print(self.term.home + self.term.white_on_blue + self.term.clear)
+                print(self.term.home + self.term.clear)
                 print(error, end="", flush=True)
                 continue
             try:
@@ -71,37 +64,29 @@ class ConnectFour:
             except ValueError:
                 error = self.term.move_xy(self.term.width//2-22, self.term.height//3) + \
                         "Both height and width must be integer values."
-                error_erase = self.term.move_xy(self.term.width//2-22, self.term.height//3) + \
-                        "                                             "
                 num_row, num_col = None, None
-                print(self.term.home + self.term.white_on_blue + self.term.clear)
+                print(self.term.home + self.term.clear)
                 print(error, end="", flush=True)
                 continue
             except TypeError:
                 error = self.term.move_xy(self.term.width//2-18, self.term.height//3) + \
                         "Please use the format HEIGHT x WIDTH."
-                error_erase = self.term.move_xy(self.term.width//2-18, self.term.height//3) + \
-                        "                                     "
                 num_row, num_col = None, None
-                print(self.term.home + self.term.white_on_blue + self.term.clear)
+                print(self.term.home + self.term.clear)
                 print(error, end="", flush=True)
                 continue
             if num_row<self.min_nRow_nCol or num_col<self.min_nRow_nCol:
                 error = self.term.move_xy(self.term.width//2-20, self.term.height//3) + \
                         f"Both height and width must be at least {self.min_nRow_nCol}."
-                error_erase = self.term.move_xy(self.term.width//2-20, self.term.height//3) + \
-                        "                                         "
                 num_row, num_col = None, None
-                print(self.term.home + self.term.white_on_blue + self.term.clear)
+                print(self.term.home + self.term.clear)
                 print(error, end="", flush=True)
                 continue
             elif num_col>self.max_nCol:
                 error = self.term.move_xy(self.term.width//2-15, self.term.height//3) + \
                         f"Width must be no more than {self.max_nCol}."
-                error_erase = self.term.move_xy(self.term.width//2-15, self.term.height//3) + \
-                        "                                        "
                 num_row, num_col = None, None
-                print(self.term.home + self.term.white_on_blue + self.term.clear)
+                print(self.term.home + self.term.clear)
                 print(error, end="", flush=True)
                 continue
             else:
@@ -109,7 +94,7 @@ class ConnectFour:
         return num_row, num_col
 
     def start(self):
-        print(self.term.home + self.term.white_on_blue + self.term.clear)
+        print(self.term.home + self.term.clear)
         ################################################################
         self.CHOICES = set(range(self.num_col))
         cur_player = 1
@@ -132,9 +117,9 @@ class ConnectFour:
         print("⎺"*(self.num_col*4+1))
         
         prompt1_p1 = self.term.move_xy(self.term.width//2-8, self.term.height//4+self.num_row+5) + \
-                     f"Player {self.style_p1('1')+self.term.white_on_blue}'s turn."
+                     f"Player {self.style_p1('1')+self.style_default}'s turn."
         prompt1_p2 = self.term.move_xy(self.term.width//2-8, self.term.height//4+self.num_row+5) + \
-                     f"Player {self.style_p2('2')+self.term.white_on_blue}'s turn."
+                     f"Player {self.style_p2('2')+self.style_default}'s turn."
         prompt1_erase = self.term.move_xy(self.term.width//2-8, self.term.height//4+self.num_row+5) + \
                         "                "
 
@@ -142,8 +127,10 @@ class ConnectFour:
                   "Choose a column by typing its letter."
         prompt2_error = self.term.move_xy(self.term.width//2-21, self.term.height//4+self.num_row+7) + \
                     f"Invalid column. Choose a column from A to {self.COL_SYMBOLS[self.num_col-1]}"
-        prompt2_erase = self.term.move_xy(self.term.width//2-21, self.term.height//4+self.num_row+7) + \
-                  "                                     "
+        prompt2_full = self.term.move_xy(self.term.width//2-23, self.term.height//4+self.num_row+7) + \
+                    f"The column is full. Choose a column from A to {self.COL_SYMBOLS[self.num_col-1]}"
+        prompt2_erase = self.term.move_xy(self.term.width//2-23, self.term.height//4+self.num_row+7) + \
+                  "                                               "
 
         with self.term.cbreak(), self.term.hidden_cursor():
             while not end:
@@ -164,33 +151,17 @@ class ConnectFour:
                             continue
                         choice = int(self.COL_SYMBOLS.index(choice_sym.upper()))
                     except ValueError:
-                        print(prompt2_erase+prompt2_error, end="", flush=True)
+                        print(prompt2_error, end="", flush=True)
                         continue
                     except EOFError:
                         print("Keyboard interrupt")
                         exit()
-                    valid = self.check_choice(choice)
-
-                print("pog")
-
-                while True:
-                    pass
+                    valid = self.check_choice(choice, prompt2_error, prompt2_full)
 
                 row = self.drop(cur_player, choice)
                 number_of_moves += 1
                 if self.check_win(cur_player, choice, row):
                     print(f'Player {cur_player} has won!')
-
-                    print(f"  {self.COL_SYMBOLS[0]}", end="")
-                    for i in range(1, self.num_col):
-                        print(f"   {self.COL_SYMBOLS[i]}", end="")
-                    print()
-                    for i in range(self.num_row):
-                        for j in range(self.num_col):
-                            print(f"│ {self.matrix[i][j]} ", end="")
-                        print("│")
-                    print("⎺"*(self.num_col*4+1))
-
                     end = True
                 if number_of_moves == total_moves:
                     print('Game is over!')
@@ -207,20 +178,37 @@ class ConnectFour:
                 return False
         return True
 
-    def check_choice(self, choice):
+    def check_choice(self, choice, prompt2_error, prompt2_full):
         if choice not in self.CHOICES:
-            print(f'Invalid column. Choose columns from A to {self.COL_SYMBOLS[choice]}')
+            print(prompt2_error, end="", flush=True)
             return False
         if self.column_is_full(choice):
-            print('The column is full. Choose another one')
+            print(prompt2_full, end="", flush=True)
             return False
         return True
 
     def drop(self, cur_player, column):
+        for i in range(self.num_row):
+            self.time.sleep(0.1)
+            if cur_player == 1:
+                disc_text = self.style_p1("1")
+            else:
+                disc_text = self.style_p2("2")
+            disc = self.term.move_xy(self.term.width//2-(self.num_col*4+1)//2+2+4*column,
+                                     self.term.height//4+1+i) + disc_text
+            disc_erase = self.term.move_xy(self.term.width//2-(self.num_col*4+1)//2+2+4*column,
+                                     self.term.height//4+i) + self.style_default("0")
+            if self.matrix[i][column] == 0:
+                print(disc)
+                if i > 0:
+                    print(disc_erase)
+            else:
+                break
         for i in reversed(range(self.num_row)):
             if self.matrix[i][column] == 0:
                 self.matrix[i][column] = cur_player
                 break
+        print(self.style_default)
         return i
 
     def check_win(self, cur_player, column, row):
@@ -257,7 +245,7 @@ class ConnectFour:
 
         # right diagonal
         count = 0
-        diag = self.np.diag(np.fliplr(self.matrix), (num_col - 1- column)-row)
+        diag = self.np.diag(self.np.fliplr(self.matrix), (self.num_col - 1- column)-row)
         for i in diag:
             if i == cur_player:
                 count += 1
